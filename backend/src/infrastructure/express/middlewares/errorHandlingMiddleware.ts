1e771db7694f1b8f0423a248d781b780beece1a9
Andrei Bereczki <andrei.bereczki@gmail.com>
2025-04-07 00:41:18 +0300

    fixed error handler

--- backend/src/infrastructure/express/middlewares/errorHandlingMiddleware.ts
+++ backend/src/infrastructure/express/middlewares/errorHandlingMiddleware.ts
@@ -1,8 +1,9 @@
-import { Request, Response } from "express";
+import { NextFunction, Response } from 'express';
 import { StatusCodes } from "http-status-codes";
 
 
-export function errorHandlingMiddleware(error: any, req: Request, res: Response) {
+export function errorHandlingMiddleware(error: any, res: Response, next: NextFunction) {
   res.status(StatusCodes.INTERNAL_SERVER_ERROR);
   res.json({ error: 'Something went wrong' });
+  console.log(error);
 }
