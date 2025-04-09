6b34072845fe4692b402b5c6482cba21743ec4a1
Andrei Bereczki <andrei.bereczki@gmail.com>
2025-04-09 11:13:44 +0300

    obi wan kenobi image

--- frontend/src/app/companies/companies.resolver.spec.ts
+++ frontend/src/app/companies/companies.resolver.spec.ts
@@ -1,17 +1,33 @@
 import { TestBed } from '@angular/core/testing';
-import { ResolveFn } from '@angular/router';
+import { ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot } from '@angular/router';
 
 import { getAllCompaniesResolver } from './companies.resolver';
-import { Company } from './companies.data-provider.service';
+import { CompaniesDataProviderService, Company } from './companies.data-provider.service';
+
+const CompaniesDataProviderServiceMock = {
+  list: jest.fn(),
+  filterById: jest.fn()
+};
 
 describe('companiesResolver', () => {
   const executeResolver: ResolveFn<Company[]> = (...resolverParameters) => TestBed.runInInjectionContext(() => getAllCompaniesResolver(...resolverParameters));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provide: CompaniesDataProviderService, useValue: CompaniesDataProviderServiceMock }]
+    });
   });
 
   it('should be created', () => {
     expect(executeResolver).toBeTruthy();
+  });
+
+  it('should return a list of companies', () => {
+    CompaniesDataProviderServiceMock.list.mockReturnValue([{ name: 'mockedCompany' }]);
+
+    const list = executeResolver({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot) as Company[];
+
+    expect(CompaniesDataProviderServiceMock.list).toHaveBeenCalled();
+    expect(list[0].name).toEqual('mockedCompany');
   });
 });
